5e46bfe2ee62aaaeadf38dd0f78264144907d32c
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-10 21:01:12 +0300

    editors model

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Editor(models.Model):
+  name = models.CharField(max_length=30)
+  email = models.EmailField()
+  phone_number = models.CharField(max_length=10, blank=True)
+
+  def __str__(self):
+      return self.name
+
+  class Meta:
+    ordering = ['name']
