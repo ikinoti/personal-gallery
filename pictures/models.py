8124c7290eebbc3b99960d7d3a450d1a52ceaf9f
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-10 21:03:42 +0300

    modified the location model class

--- pictures/models.py
+++ pictures/models.py
@@ -16,5 +16,20 @@ class Editor(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length =60)
 
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
     def __str__(self):
         return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
