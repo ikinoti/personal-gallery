901e1f88dbc2b032e74489611dc2b5bf4bea9fb1
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-10 21:04:22 +0300

    created category model class

--- pictures/models.py
+++ pictures/models.py
@@ -33,3 +33,15 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
