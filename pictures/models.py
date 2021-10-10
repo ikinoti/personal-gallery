7f2ed593b69b8243d12471bf4adf693011f1ca06
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-10 21:08:07 +0300

    modify image model class

--- pictures/models.py
+++ pictures/models.py
@@ -52,8 +52,36 @@ class Image(models.Model):
     description = models.TextField()
     author = models.CharField(max_length=40, default='kinoti')
     date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category)
-    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
