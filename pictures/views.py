1ada8d63d2f65af4d940696c987b9a3c99fe1563
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-10 21:34:00 +0300

    imge location view function

--- pictures/views.py
+++ pictures/views.py
@@ -8,3 +8,8 @@ def index(request):
     locations = Location.get_locations()
     print(locations)
     return render(request, 'pictures/index.html', {'locations': locations})
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pictures/location.html', {'location_images': images})
